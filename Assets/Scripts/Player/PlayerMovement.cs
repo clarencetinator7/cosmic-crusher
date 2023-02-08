b4b12385bbba9d9a56b55b296c6979c25696ff7b
clarencetinator7 <clarencejoseph.dimafelix@gmail.com>
2023-02-08 19:48:09 +0800

    add player slow while charging attack

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,39 +7,65 @@ public class PlayerMovement : MonoBehaviour
 {
     private PlayerInputAction inputActions;
     private InputAction movement;
+    private InputAction fire;
     private Rigidbody2D rb;
 
     [SerializeField]
-    private float movementSpeed = 5f;
+    private float movementSpeed = 1f;
     [SerializeField]
     private float maxVelocity = 5f;
+    [SerializeField]
+    private float acceleration = 0.1f;
     private Vector2 moveDirection;
     private Vector2 newVelocity;
-    
+        
+    private bool isFiring = false;
+
     private void Awake() {
         inputActions = new PlayerInputAction();
         movement = inputActions.Player.Move;
+        fire = inputActions.Player.Fire;
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnEnable() {
         movement.Enable();
+
+        fire = inputActions.Player.Fire;
+        fire.Enable();
+        fire.performed += OnAttack; 
+        fire.canceled += OnAttack;
     }
 
     private void Update() {
-        // This code is for rotating the player towards the direction of movement
-        // if (moveDirection != Vector2.zero) {
-        //     float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
-        //     transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        // }
         moveDirection = movement.ReadValue<Vector2>();
-        newVelocity = moveDirection * movementSpeed;
+        // newVelocity = moveDirection * movementSpeed;
+        if(!isFiring) {
+            newVelocity += moveDirection * movementSpeed;
+            maxVelocity = 7f;
+        } else {
+            // Slow down the player while charging the attack
+            newVelocity = moveDirection * 0.5f;
+            maxVelocity = 0.5f;
+        }
     }
 
     private void FixedUpdate() {
-        // rb.velocity = newVelocity;
-
+        rb.velocity = newVelocity;
+        // Limit the velocity of the player
         rb.velocity = Vector2.ClampMagnitude(rb.velocity + newVelocity, maxVelocity);
+        // Slow down the player
         rb.velocity = Vector2.Lerp(rb.velocity, Vector2.zero, 0.1f);
     }
+
+    private void OnAttack(InputAction.CallbackContext context) {
+
+        if(context.performed) {
+            isFiring = true;
+        } else if (context.canceled) {
+            isFiring = false;
+        }
+
+    }
+
 }
